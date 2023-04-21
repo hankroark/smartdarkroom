2e338db302569b3cb342c10730cbc7b6121b8f01
Hank Roark <hank.roark@gmail.com>
2023-04-21 16:04:12 -0700

    change stop steps in test strips to be fractional

--- smartdarkroom/prints.py
+++ smartdarkroom/prints.py
@@ -17,8 +17,8 @@ class OneExposurePrint(BasicPrint):
 
 
 class FStopTestStrip(BasicPrint):
-    def __init__(self, base=4, steps=5, stops=2):
-        total_steps = [ base * 2**(i/stops) for i in range(0,steps) ]
+    def __init__(self, base=4, steps=5, stops=1/2):
+        total_steps = [ base * 2**(i*stops) for i in range(0,steps) ]
         total_steps.insert(0, 0)
         incremental_steps = [j-i for i, j in zip(total_steps[:-1], total_steps[1:])]  # take differences between steps
         steps = [ {"duration":i, 
@@ -28,8 +28,8 @@ class FStopTestStrip(BasicPrint):
 
 
 class LocalizedFStopTestStrip(BasicPrint):
-    def __init__(self, base=4, steps=5, stops=2):
-        steps = [ {"duration":base * 2**(i/stops), 
+    def __init__(self, base=4, steps=5, stops=1/2):
+        steps = [ {"duration":base * 2**(i*stops), 
                    "user_prompt": "Place paper for print." if i==0 else "Move paper to expose next strip."} 
                    for i in range(0, steps) ] 
         super().__init__(steps = steps)
